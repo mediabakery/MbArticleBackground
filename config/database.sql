eda0fdb34e1bb5a789e2aada8e0b51a6a1189ca3
mediabakery <seb.tilch@gmx.de>
2012-05-31 10:07:30 +0200

    Article Background for contao

---
+++ config/database.sql
@@ -0,0 +1,18 @@
+-- ********************************************************
+-- *                                                      *
+-- * IMPORTANT NOTE                                       *
+-- *                                                      *
+-- * Do not import this file manually but use the Contao  *
+-- * install tool to create and maintain database tables! *
+-- *                                                      *
+-- ********************************************************
+
+-- 
+-- Table `tl_article`
+-- 
+
+CREATE TABLE `tl_article` (
+  `mb_articlebackground` char(1) NOT NULL default '',
+  `mb_articlebackground_src` varchar(255) NOT NULL default '',
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
